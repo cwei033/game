3dd67c71491771dbc916c6d71b7859808ed01ca4
Julie <60452220+cwei033@users.noreply.github.com>
2020-11-17 15:00:00 -0800

    Update sketch.js

--- posenet/sketch.js
+++ posenet/sketch.js
@@ -2,6 +2,7 @@ let video;
 let poseNet;
 let pose;
 let bird;
+let skeleton;
 
 function preload() {
   bird = loadImage('bird.png')
@@ -16,9 +17,10 @@ function setup() {
 }
 
 function gotPoses(poses) {
-  console.log(poses);
+  // console.log(poses);
   if (poses.length > 0) {
     pose = poses[0].pose;
+    skeleton = poses[0].skeleton;
   }
 }
 
@@ -38,5 +40,19 @@ function draw() {
     fill(255, 0, 0);
     ellipse(pose.nose.x, pose.nose.y, d);
     image(bird, pose.leftShoulder.x, pose.leftShoulder.y, 80, 80);
+
+    // for (let i = 0; i < pose.keypoints.length; i++) {
+    //   let x = pose.keypoints[i].position.x;
+    //   let y = pose.keypoints[i].position.y;
+    //   fill('orange');
+    //   ellipse(x, y, 16, 16);
+    // }
+
+    for (let i = 0; i < skeleton.length; i++) {
+      let a = skeleton[i][0];
+      let b = skeleton[i][1];
+      strokeWeight(2);
+      line(a.position.x, a.position.y, b.position.x, b.position.y);
+    }
   }
 }
